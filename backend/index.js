de829476c698f239a0ee9a910dffac0aa9d66123
riadhh-302005 <30ria10@gmail.com>
2026-03-18 23:47:06 +0530

    fix cors for production

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,9 @@ const port = process.env.PORT || 4000;
 
 // ⭐ IMPORTANT: ENABLE CREDENTIALS FOR CLERK COOKIE SESSION
 const allowedOrigins = [
-  "http://localhost:5173", // user frontend
-  "http://localhost:5174", // admin dashboard
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://medicare-project-ten.vercel.app",
 ];
 
 app.use(
